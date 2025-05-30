41cc9cbb474877a0158e76b90f3b55471be67c41
Vineeth-0509 <vineeththungani@gmail.com>
2025-05-30 23:44:29 +0530

    "linting-fixex"

--- app/chats/[id]/page.tsx
+++ app/chats/[id]/page.tsx
@@ -4,14 +4,27 @@ import { fetchChatGroup, fetchChatGroupUsers } from "@/fetch/groupFetch";
 import { ChatGroupType, GroupChatUserType, MessageType } from "@/types/types";
 import { notFound } from "next/navigation";
 
-import React from "react";
+// import React from "react";
+// interface PageProps{
+//   params:{
+//     id:string;
+//   };
+//   searchParams?:{
+//     [key:string]: string | string[] | undefined
+//   }
+//}
 
-export default async function chat({ params }: { params: { id: string } }) {
-  const groupId = params.id;
-  if (groupId.length !== 36) {
+import { Metadata } from "next";
+type Props = {
+  params:{id: string};
+  
+}
+export default async function Page(props: any) {
+  const { params } = props;
+  if (params.id.length !== 36) {
     return notFound();
   }
-  const group: ChatGroupType | null = await fetchChatGroup(groupId);
+  const group: ChatGroupType | null = await fetchChatGroup(params.id);
   if (group === null) {
     return notFound();
   }
